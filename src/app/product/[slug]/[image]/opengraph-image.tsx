48da030de2f1db83362b24457170a083aaf5fd51
Maicon <devmaicon@gmail.com>
2023-10-30 15:40:38 -0300

    update fetch and opengraph-image

--- src/app/product/[slug]/[image]/opengraph-image.tsx
+++ src/app/product/[slug]/[image]/opengraph-image.tsx
@@ -1,63 +1,63 @@
-// /* eslint-disable @next/next/no-img-element */
-// import { ImageResponse } from 'next/og'
+/* eslint-disable @next/next/no-img-element */
+import { ImageResponse } from 'next/og'
 
-// import colors from 'tailwindcss/colors'
-// import { fetchAuthenticated } from '@/lib/fetch-authenticated'
-// import { Product } from '@prisma/client'
-// // Route segment config
-// export const runtime = 'edge'
+import colors from 'tailwindcss/colors'
+import { fetchAuthenticated } from '@/lib/fetch-authenticated'
+import { Product } from '@prisma/client'
+// Route segment config
+export const runtime = 'edge'
 
-// // Image metadata
-// // export const alt = 'About Acme'
-// export const size = {
-//     width: 1200,
-//     height: 630,
-// }
+// Image metadata
+// export const alt = 'About Acme'
+export const size = {
+    width: 1200,
+    height: 630,
+}
 
-// export const contentType = 'image/png'
+export const contentType = 'image/png'
 
-// // Image generation
-// export async function getProduct(slug: string): Promise<Product> {
-//     const response = await fetchAuthenticated(`/products/${slug}`, { next: { tags: ["product"] } });
-//     return await response.json();
-// }
-
-
-
-// export default async function OgImage({ params }: { params: { slug: string } }) {
-
-//     const product = await getProduct(params.slug);
-
-//     return new ImageResponse(
-//         (
-//             // ImageResponse JSX element
-//             <div
-//                 style={{
-//                     background: colors.zinc[950],
-//                     width: '100%',
-//                     height: '100%',
-//                     display: 'flex',
-//                     flexDirection: 'column'
-//                 }}
-//             >
-//                 <img
-//                     src={product.imageUrls[0]}
-//                     alt={product.name}
-//                     style={{
-//                         objectFit: 'contain',
-//                         width: '100%',
-//                         height: '100%',
-//                     }}
+// Image generation
+export async function getProduct(slug: string): Promise<Product> {
+    const response = await fetch(`/products/${slug}`, { next: { tags: ["product"] } });
+    return await response.json();
+}
 
 
-//                 />
-//             </div>
-//         ),
-//         // ImageResponse options
-//         {
-//             // For convenience, we can re-use the exported opengraph-image
-//             // size config to also set the ImageResponse's width and height.
-//             ...size
-//         }
-//     )
-// }
+
+export default async function OgImage({ params }: { params: { slug: string } }) {
+
+    const product = await getProduct(params.slug);
+
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    background: colors.zinc[950],
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column'
+                }}
+            >
+                <img
+                    src={product.imageUrls[0]}
+                    alt={product.name}
+                    style={{
+                        objectFit: 'contain',
+                        width: '100%',
+                        height: '100%',
+                    }}
+
+
+                />
+            </div>
+        ),
+        // ImageResponse options
+        {
+            // For convenience, we can re-use the exported opengraph-image
+            // size config to also set the ImageResponse's width and height.
+            ...size
+        }
+    )
+}
---
+++ src/lib/fetch.ts
@@ -0,0 +1,19 @@
+import { env } from '@/env';
+
+export async function fetch(path: string, init?: RequestInit | undefined) {
+  const baseUrl = env.NEXT_PUBLIC_URL_API;
+  const url = baseUrl + path;
+
+
+  const headers = {
+    'Content-Type': 'application/json',
+    ...init?.headers,
+  } as HeadersInit | undefined;
+
+  const options = {
+    ...init,
+    headers,
+  } as RequestInit | undefined;
+
+  return fetch(url, options);
+}
